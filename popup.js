fddb7688dd8b35ab4c63ad605b1e7d6bec93d069
Hassaan Tauqeer <syed.hassaan.tauqeer@gmail.com>
2022-03-03 19:10:32 +0100

    main functionality

--- popup.js
+++ popup.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dialogBox = document.getElementById('dialog-box');
+    document.body.style.width = '400px';
+
+    const beatSize = document.getElementById('beat-size');
+    const beatStats = document.getElementById('beat-stats');
     const query = { active: true, currentWindow: true };
 
     chrome.tabs.query(query, (tabs) => {
-        console.log("****** tabs: ", tabs)
-        dialogBox.innerHTML = getBarkedTitle(tabs[0].title);
+        getSummary(tabs[0], beatSize, beatStats)
     });
 });
 
-const getBarkedTitle = (tabTitle) => {
-    const barkTitle = `${getRandomBark()} Ahem.. I mean, we are at: ${tabTitle}`
-    return barkTitle;
-}
+const getSummary = (tab, beatSize, beatStats) => {
+    chrome.tabs.executeScript(tab.id, {file: "inject.js"}, function(summary){
+        elements = summary[0]
+        console.log(elements)
+        beatSize.innerHTML = `Total beat size points: ${elements.sizeStats}`
+        initStr = ""
+        var k = Object.keys(elements.statusStats);
+        var v = Object.values(elements.statusStats);
+        k.forEach((key, index) => {
+            initStr = initStr + `<tr><th>`+ key + `</th><th>` + v[index] + `</th></tr>`
+        });
+        beatStats.innerHTML = `<table>` + initStr + `</table>`
 
-const barks = [
-    'Barf barf!',
-    'Birf birf!',
-    'Woof woof!',
-    'Arf arf!',
-    'Yip yip!',
-    'Biiiirf!'
-]
-
-const getRandomBark = () => {
-    const bark = barks[Math.floor(Math.random() * barks.length)];
-    return bark;
+    });
 }
+{/* <tr>${JSON.stringify(elements.typeStats, null, 4)}</tr> */}
+{/* <tr>${JSON.stringify(elements.repoStats)}</tr> */}
